e19bc8ed37133447e83998d0e4a9cad4be2812a7
RandelChannie <growupskzchanwolf@gmail.com>
2025-05-24 21:05:06 -0500

    Subiendo modificación que hizo dev2-V

--- controlador/index.php
+++ controlador/index.php
@@ -1,20 +1,61 @@
 <?php
-require_once "modelo/Producto.php";
+require_once("modelo/index.php");
 
-$producto = new Producto();
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST["agregar"])) {
-        $producto->insertar($_POST["nombre"], $_POST["precio"]);
-        header("Location: index.php");
-    } elseif (isset($_POST["editar"])) {
-        $producto->actualizar($_POST["id"], $_POST["nombre"], $_POST["precio"]);
-        header("Location: index.php");
+class modelController {
+    // Attributes do in class
+    private $model;
+    
+    public function __construct() {
+        $this->model = new Modelo();
+    }
+    
+    // mostrar
+    static function index() {
+        $producto = new Modelo();
+        $data = $producto->mostrar("products", "1");
+        require_once("vista/index.php");
+    }
+    
+    // nuevo
+    static function nuevo() {
+        require_once("vistas/nuevo.php");
+    }
+    
+    // guardar
+    static function guardar() {
+        $nombre = $_REQUEST['nombre'];
+        $precio = $_REQUEST['precio'];
+        $data = "'".$nombre."','".$precio."'";
+        $producto = new Modelo();
+        $data = $producto->insertar("products", $data);
+        header("location:".urlsite);
+    }
+    
+    // editar
+    static function editar() {
+        $id = $_REQUEST['id'];
+        $producto = new Modelo();
+        $data = $producto->mostrar("products", "id=".$id);
+        require_once("vistas/editar.php");
+    }
+    
+    // actualizar
+    static function actualizar() {
+        $id = $_REQUEST['id'];
+        $nombre = $_REQUEST['nombre'];
+        $precio = $_REQUEST['precio'];
+        $data = "nombre='".$nombre."', precio=".$precio;
+        $producto = new Modelo();
+        $data = $producto->actualizar("products", $data, "id=".$id);
+        header("location:".urlsite);
+    }
+    
+    // eliminar
+    static function eliminar() {
+        $id = $_REQUEST['id'];
+        $producto = new Modelo();
+        $data = $producto->eliminar("products", "id=".$id);
+        header("location:".urlsite);
     }
 }
-
-if (isset($_GET["eliminar"])) {
-    $producto->eliminar($_GET["eliminar"]);
-    header("Location: index.php");
-}
-?>
+//
---
+++ modelo/index.php
@@ -0,0 +1,21 @@
+
+<?php
+require_once "modelo/Producto.php";
+
+$producto = new Producto();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["agregar"])) {
+        $producto->insertar($_POST["nombre"], $_POST["precio"]);
+        header("Location: index.php");
+    } elseif (isset($_POST["editar"])) {
+        $producto->actualizar($_POST["id"], $_POST["nombre"], $_POST["precio"]);
+        header("Location: index.php");
+    }
+}
+
+if (isset($_GET["eliminar"])) {
+    $producto->eliminar($_GET["eliminar"]);
+    header("Location: index.php");
+}
+?>
