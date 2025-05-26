ce7690909dfd719d4fd3efcd4dd37251293791dd
Lincol-Torres <torres17061999@gmail.com>
2025-05-26 15:14:27 -0500

    This reverts commit 464c44d219491a7363471cf79f1f25a1ec22b0f9.

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<?php
-require_once("config.php");
-require_once("controlador/index.php");
-
-// ❌ Simulación de error: función inexistente
-simulacionDeErrorFatal();
-
-if(isset($_GET['m'])):
-    if(method_exists("modeloController", $_GET['m'])):
-        modeloController::{$_GET['m']}();
-    endif;
-else:
-    modeloController::index();
+<?php 
+require_once("config.php"); 
+require_once("controlador/index.php"); 
+if(isset($_GET['m'])): 
+	if(method_exists("modeloController", $_GET['m'])): 
+		modeloController:: {$_GET['m']}(); 
+	endif; 
+else: 
+	modeloController::index(); 
 endif;
 ?>
